69475d9956ddd0d2868cf9379bf4a84816c77582
Atsushi Watanabe <atsushi.w@ieee.org>
2023-03-31 15:45:46 +0900

    Fallback to dnf info without available flag on finding source rpm

--- entrypoint.sh
+++ entrypoint.sh
@@ -87,7 +87,12 @@ while read package; do
   pkg_version=$(echo ${pkg_fields} | cut -f2 -d" ")
   pkg_suffix=$(echo ${pkg_fields} | cut -f3 -d" ")
 
-  pkg_src=$(dnf info --available ${pkg_name} | sed -n 's/^Source\s*:\s*\(\S\+\).src.rpm/\1/p')
+  pkg_src=$(
+    (
+      dnf info --available ${pkg_name} \
+        || dnf info ${pkg_name}
+    ) | sed -n 's/^Source\s*:\s*\(\S\+\).src.rpm/\1/pq'
+  )
   src_fields=$(split_package_name ${pkg_src})
   src_name=$(echo ${src_fields} | cut -f1 -d" ")
   for pkg_arch in ${arch} noarch; do
